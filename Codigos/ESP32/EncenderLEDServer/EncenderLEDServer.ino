36ff235f20470cc7a3090b42d5d42619619f72eb
Mane2302 <95459596+Mane2302@users.noreply.github.com>
2026-04-26 22:26:10 -0600

    Update EncenderLEDServer.ino

--- Codigos/ESP32/EncenderLEDServer/EncenderLEDServer.ino
+++ Codigos/ESP32/EncenderLEDServer/EncenderLEDServer.ino
@@ -1,19 +1,24 @@
-#include <WiFiManager.h>
-#include <WiFi.h>
-#include <WebServer.h>
+#include <WiFi.h> // Control de WiFi en ESP32
+#include <WebServer.h> // Servidor web interno
+ 
+ //VARIABLES
+int LEDR = 14; //LED rojo en pin DGPIO 14
+WebServer server(80); // Servidor web en puerto 80 (HTTP)
 
-int LEDR = 14;
-WebServer server(80);
 
+//PÁGINA WEB
 void handleRoot() {
-  String html = "<html><body style='text-align:center;'>"
+  String html = "<html><body style='text-align:center;'>" //centramos todo en la pagina y escalamos
+                "<meta name='viewport' content='width=device-width,initial-scale=1'>"
                 "<h1>Control LED</h1>"
-                "<button onclick=\"fetch('/on')\">Encender</button>"
+                "<button onclick=\"fetch('/on')\">Encender</button>" //fetch hace una petición a la ESP32 llamando a /on
                 "<button onclick=\"fetch('/off')\">Apagar</button>"
                 "</body></html>";
-  server.send(200, "text/html", html);
+  server.send(200, "text/html", html); //(codigo HTTP OK, contenido, lo que se muestra)
 }
 
+
+//PETICIONES
 void handleOn() {
   digitalWrite(LEDR, HIGH); // prende
   server.send(200, "text/plain", "ON");
@@ -24,25 +29,27 @@ void handleOff() {
   server.send(200, "text/plain", "OFF");
 }
 
+
 void setup() {
-  WiFi.mode(WIFI_STA);
+  //WiFi.mode(WIFI_STA);
   Serial.begin(115200);
 
-  pinMode(LEDR, OUTPUT);
-  digitalWrite(LEDR, HIGH);
+  pinMode(LEDR, OUTPUT); //pin como salida
+  digitalWrite(LEDR, LOW); //iniciamos con el pin apagado
 
-  WiFiManager wm;
- WiFi.softAP("ESP32_LED", "12345678");
- Serial.println(WiFi.softAPIP());
+  //creamos nuestra propia RED con softAP
+  WiFi.softAP("ESP32_LED", "12345678");
+  Serial.println(WiFi.softAPIP());
 
-  Serial.println(WiFi.localIP());
-
+  //Define qué función se ejecuta según la URL
   server.on("/", handleRoot);
   server.on("/on", handleOn);
   server.on("/off", handleOff);
-  server.begin();
+
+  server.begin(); //activamos el servidor
 }
 
 void loop() {
-  server.handleClient();
+
+  server.handleClient(); //se ejecuta constantemente para recibir las peticiones de la web
 }
